c0de9b1de75eeb5f16d144f84abab8709d6288b4
stevempt <steve+github@masonpearsontechnologies.com>
2019-05-31 16:56:57 -0400

    might need to switch to using enzyme.mount and rendering hook in a component to fully test (instead of jook, due to context provider)

--- __tests__/store/cart/useCart.test.js
+++ __tests__/store/cart/useCart.test.js
@@ -1,26 +1,48 @@
 import init from "jooks"
 import { addProductToCart } from "../../../src/store/cart/actions"
 import store from "../../../src/store/cart/persistentStore"
-import { useCart } from "../../../src/store/cart/useCart"
+import { getCartSummary } from "../../../src/store/cart/reducer"
+import { CartStoreContext, useCart } from "../../../src/store/cart/useCart"
 
 jest.mock("../../../src/store/cart/persistentStore", () => ({
   get: jest.fn().mockImplementation(() => []),
   set: jest.fn(),
 }))
 
+const mockItem = {
+  sku: "fake-sku",
+  title: "fake-title",
+  price: 999.99,
+  image: "fake-image-url",
+  qty: 1,
+}
+
+const mockState = {
+  items: [mockItem],
+}
+
 describe("useCart hook", () => {
   const mockHook = init(() => useCart())
+  const mockDispatch = jest.fn()
 
-  it("uses store for initial state", () => {
-    mockHook.run()
-    expect(store.get).toHaveBeenCalledTimes(1)
+  beforeEach(() => {
+    mockHook.setContext(CartStoreContext, {
+      state: mockState,
+      dispatch: mockDispatch,
+    })
   })
 
   it("updates store after state changes", async () => {
     await mockHook.mount()
     const [, dispatch] = mockHook.run()
-    dispatch(addProductToCart({ sku: 1 }))
+    dispatch(addProductToCart(mockItem))
     await mockHook.wait()
-    expect(store.set).toHaveBeenLastCalledWith([{ sku: 1, qty: 1 }])
+    expect(store.set).toHaveBeenLastCalledWith(mockState.items)
+  })
+
+  it("state includes computed summary", async () => {
+    await mockHook.mount()
+    const [state] = mockHook.run()
+    expect(state.summary).toEqual(getCartSummary(mockState.items))
   })
 })
--- src/store/cart/useCart.js
+++ src/store/cart/useCart.js
@@ -1,14 +1,14 @@
-import React, { createContext, useContext, useEffect, useReducer } from "react"
+import React from "react"
 import store from "./persistentStore"
 import reducer, { getCartSummary } from "./reducer"
 
 // Create a "global" context to store our redux state in
-const CartStoreContext = createContext()
+const CartStoreContext = React.createContext()
 
 // Use this to wrap the whole portion of the app that needs access to cart
 export const CartStoreProvider = ({ children }) => {
   const initialState = { items: store.get() }
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = React.useReducer(reducer, initialState)
   return (
     <CartStoreContext.Provider value={{ state, dispatch }}>
       {children}
@@ -19,9 +19,9 @@ export const CartStoreProvider = ({ children }) => {
 // Use this to access cart state and dispatch functions, like useReducer
 // except it is available locally anywhere under the Provider wrapper
 export const useCart = () => {
-  const { state, dispatch } = useContext(CartStoreContext)
+  const { state, dispatch } = React.useContext(CartStoreContext)
 
-  useEffect(() => {
+  React.useEffect(() => {
     store.set(state.items)
   }, [state])
 
